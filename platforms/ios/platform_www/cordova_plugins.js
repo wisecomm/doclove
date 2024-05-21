ccd6bb5da47e550c013cae1d143a42732540e400
danyoh <danyoh@gmail.com>
2024-05-21 13:21:19 +0900

    cordova-plugin-screen-orientation 추가

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-screen-orientation.screenorientation",
+      "file": "plugins/cordova-plugin-screen-orientation/www/screenorientation.js",
+      "pluginId": "cordova-plugin-screen-orientation",
+      "clobbers": [
+        "cordova.plugins.screenorientation",
+        "screen.orientation"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-screen-orientation": "3.0.4"
+  };
+});
