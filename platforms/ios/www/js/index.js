05bb3c98c30981e88dcbad2d6bc95551aa506afa
danyoh <danyoh@gmail.com>
2024-05-21 14:25:28 +0900

    화면 락 추가

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -23,6 +23,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
+    screen.orientation.lock('portrait');
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
